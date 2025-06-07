de03566cd3da01f39cfb51cc81cab3f77f790731
“notURandomDev” <1250901577@qq.com>
2025-06-07 12:10:27 +0800

    feat：项目初始化

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  pages: [
+  'pages/index/index', "pages/reserve/index"],
+
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+});
