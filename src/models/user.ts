75d92227fdcbcda591be2f64ee79dffceca6ed0b
“notURandomDev” <1250901577@qq.com>
2025-06-13 12:02:36 +0800

    feat：数据模型定义-用户

---
+++ src/models/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+  _id: string | number;
+  openid?: string;
+  avatarFileID?: string | null;
+  avatarUrl?: string | null;
+  nickName?: string | null;
+  roles?: BandRole[];
+}
+
+enum BandRole {
+  Drummer = "drummer",
+  Guitarist = "guitarist",
+  Bassist = "bassist",
+  Keyboardist = "keyboardist",
+  Vocalist = "vocalist",
+}
