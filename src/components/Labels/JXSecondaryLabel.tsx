7a4cf07c4154f8652712c7922e8b0a3ed723fb81
notURandomDev <1250901577@qq.com>
2025-06-14 01:44:08 +0800

    chore：清除冗余依赖

--- src/components/Labels/JXSecondaryLabel.tsx
+++ src/components/Labels/JXSecondaryLabel.tsx
@@ -1,5 +1,4 @@
 import { Text } from "@tarojs/components";
-import React from "react";
 
 function JXSecondaryLabel({ children }: { children?: string }) {
   return <Text style={{ fontSize: 12, color: "#94989b" }}>{children}</Text>;
