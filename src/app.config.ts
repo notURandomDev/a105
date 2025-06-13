47304932f4d479a1c160aa774d05708b9cb6e5b9
“notURandomDev” <1250901577@qq.com>
2025-06-13 10:39:37 +0800

    feat：添加 tabbar 图标资源

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,46 @@
 export default defineAppConfig({
   pages: [
-  'pages/index/index', "pages/reserve/index"],
+    "pages/table/index",
+    "pages/reserve/index",
+    "pages/profile-edit/index",
+    "pages/index/index",
+    "pages/profile/index",
+    "pages/auth/index",
+  ],
 
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
+    backgroundColor: "#fafcff",
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+
+  tabBar: {
+    color: "#999", // 默认文字颜色
+    selectedColor: "#000", // 选中时文字颜色
+    backgroundColor: "#FFF", // 背景色
+    borderStyle: "white",
+    list: [
+      // 导航项列表
+      {
+        pagePath: "pages/index/index", // 页面路径（基于src目录）
+        text: "首页", // 显示文字
+        iconPath: "assets/icons/home.png",
+        selectedIconPath: "assets/icons/home-active.png",
+      },
+      {
+        pagePath: "pages/table/index",
+        text: "排练预约",
+        iconPath: "assets/icons/calendar.png",
+        selectedIconPath: "assets/icons/calendar-active.png",
+      },
+      {
+        pagePath: "pages/profile/index",
+        text: "我的",
+        iconPath: "assets/icons/profile.png",
+        selectedIconPath: "assets/icons/profile-active.png",
+      },
+    ],
+  },
 });
