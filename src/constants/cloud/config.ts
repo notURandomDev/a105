03e4ee205ac646d83253b5cd2775d44366545888
“notURandomDev” <1250901577@qq.com>
2025-06-13 11:56:28 +0800

    feat：响应成功信息常量：云服务相关（云函数、云存储）

---
+++ src/constants/cloud/config.ts
@@ -0,0 +1,6 @@
+export const CLOUD_CODE_ERRMSG_OK = "login:ok";
+export const CLOUD_OPENID_ERRMSG_OK = "cloud.callFunction:ok";
+export const CLOUD_USER_PROFILE_OK = "getUserProfile:ok";
+export const CLOUD_UPLOAD_OK = "document.update:ok";
+export const CLOUD_GET_FILE_OK = "cloud.getTempFileURL:ok";
+export const CLOUD_CALL_FUNCTION_OK = "cloud.callFunction:ok";
