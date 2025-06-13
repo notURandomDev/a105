ddc68209790ed1436184dd69fc1fb4633bfc908b
“notURandomDev” <1250901577@qq.com>
2025-06-13 15:38:01 +0800

    chore：测试git邮箱绑定

--- src/cloud/cloudClient.ts
+++ src/cloud/cloudClient.ts
@@ -1,7 +1,7 @@
 import Taro from "@tarojs/taro";
 
 Taro.cloud.init({
-  env: process.env.TARO_CLOUD_ENV_ID, // 当前的云开发环境 ID
+  env: process.env.TARO_CLOUD_ENV_ID, // 当前的腾讯云开发环境 ID
 });
 
 export const db = Taro.cloud.database();
