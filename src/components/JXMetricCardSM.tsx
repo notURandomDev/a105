0412a9ff6c86ea72cdff3cbc473fc7096a4ac581
notURandomDev <1250901577@qq.com>
2025-06-15 23:19:52 +0800

    feat：小型数据展示卡片-新增｜3列数据展示场景使用

---
+++ src/components/JXMetricCardSM.tsx
@@ -0,0 +1,32 @@
+import JXCardContainer from "./JXCardContainer";
+import { Text, View } from "@tarojs/components";
+
+interface JXMetricCardSMProps {
+  label?: string;
+  value?: string | number;
+  emoji?: string;
+  active?: boolean;
+}
+function JXMetricCardSM({
+  label = "标题",
+  value = 0,
+  emoji = "⭐️",
+  active = false,
+}: JXMetricCardSMProps) {
+  return (
+    <JXCardContainer color={active ? "black" : "gray"} className="grow">
+      <View
+        className="container-h grow"
+        style={{ alignItems: "center", gap: 12 }}
+      >
+        <Text style={{ fontSize: 24 }}>{emoji}</Text>
+        <Text style={{ fontWeight: 600, fontSize: 28 }}>{value}</Text>
+      </View>
+      <View className="container-v">
+        <Text style={{ fontSize: 14 }}>{label}</Text>
+      </View>
+    </JXCardContainer>
+  );
+}
+
+export default JXMetricCardSM;
