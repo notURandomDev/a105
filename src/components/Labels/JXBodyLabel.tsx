25f90e56d278824859ac8bbdee9c415afb3aec75
notURandomDev <1250901577@qq.com>
2025-06-15 23:27:51 +0800

    feat：正文标签-新增

---
+++ src/components/Labels/JXBodyLabel.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@tarojs/components";
+
+function JXBodyLabel({ children }: { children?: string }) {
+  return <Text style={{ fontSize: 12 }}>{children}</Text>;
+}
+
+export default JXBodyLabel;
