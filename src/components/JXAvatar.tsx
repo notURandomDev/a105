8340fe13c4f0c4f3d0949bfb3ade8dd47b549c9f
notURandomDev <1250901577@qq.com>
2025-06-15 23:16:28 +0800

    feat：头像组件-完善｜添加style参数支持

--- src/components/JXAvatar.tsx
+++ src/components/JXAvatar.tsx
@@ -1,18 +1,25 @@
 import { Avatar } from "@taroify/core";
 import { AvatarShape, AvatarSize } from "@taroify/core/avatar/avatar.shared";
+import { CSSProperties } from "react";
 
 interface JXAvatarProps {
   children: string | null | undefined;
   size?: AvatarSize;
   shape?: AvatarShape;
+  style?: CSSProperties;
 }
 function JXAvatar({
   children,
   size = "medium",
   shape = "circle",
+  style,
 }: JXAvatarProps) {
   return (
-    <Avatar size={size} shape={shape} style={{ backgroundColor: "black" }}>
+    <Avatar
+      size={size}
+      shape={shape}
+      style={{ backgroundColor: "black", ...style }}
+    >
       {children ? children[0].toUpperCase() : "?"}
     </Avatar>
   );
