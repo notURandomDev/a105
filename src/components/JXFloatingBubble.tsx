b2096b65fbfdc334324bf1e4afcbf84531083249
notURandomDev <1250901577@qq.com>
2025-06-15 23:18:49 +0800

    feat：FAB优化｜添加offset参数的传入

--- src/components/JXFloatingBubble.tsx
+++ src/components/JXFloatingBubble.tsx
@@ -1,14 +1,19 @@
 import { FloatingBubble } from "@taroify/core";
+import { FloatingBubbleOffset } from "@taroify/core/floating-bubble/floating-bubble.shared";
 import { Plus } from "@taroify/icons";
 
-interface JXFloatingBubble {
-  onClick: () => void;
+interface JXFloatingBubbleProps {
+  onClick?: () => void;
+  offset?: FloatingBubbleOffset;
 }
 
-function JXFloatingBubble({ onClick }) {
+function JXFloatingBubble({
+  onClick,
+  offset = { x: -1, y: -1 },
+}: JXFloatingBubbleProps) {
   return (
     <FloatingBubble
-      offset={{ x: -1, y: 450 }}
+      offset={offset}
       magnetic="x"
       axis="xy"
       style={{ backgroundColor: "black" }}
