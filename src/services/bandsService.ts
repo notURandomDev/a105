69c68ecbc65f27b1a343b74040771ef5383151f3
notURandomDev <1250901577@qq.com>
2025-06-18 20:19:59 +0800

    feat：乐队数据库请求函数｜更新
    - 添加了用于新增乐队的函数：createBand
    - 添加了能够根据乐队状态获取乐队数据的函数：getBandsByStatus

--- src/services/bandsService.ts
+++ src/services/bandsService.ts
@@ -1,30 +1,51 @@
 import { _, db } from "@/cloud/cloudClient";
 import { MOCK_BANDS } from "@/constants/database/bands";
-import { DB_ERRMSG_OK } from "@/constants/database/config";
-import { Band } from "@/models/band";
+
+import { Band, BandStatus, CreateBandInput } from "@/models/band";
+import { handleDBResult } from "@/utils/database";
+import { DB } from "@tarojs/taro";
 
 const bandsCollection = db.collection("band");
 /* CREATE */
 
-/* READ */
-
-export const getAllBands = async ({
-  production = false,
-}: {
+interface CreateBandParams {
+  band: CreateBandInput;
   production?: boolean;
-} = {}): Promise<Band[] | null> => {
-  const QUERY_ALL = "获取全部乐队数据";
-
-  if (!production) return MOCK_BANDS.DEFAULT;
+}
+export const createBand = async ({
+  band,
+  production = false,
+}: CreateBandParams): Promise<boolean | null> => {
+  if (!production) return true;
 
   try {
-    const res = await bandsCollection.get();
+    const res = await bandsCollection.add({ data: band });
 
-    if (res.errMsg !== DB_ERRMSG_OK) {
-      throw new Error(QUERY_ALL + `失败：${res.errMsg}`);
-    }
+    handleDBResult(res, "get", "创建乐队记录");
 
-    console.log(QUERY_ALL + "成功：", res);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+/* READ */
+
+interface GetAllBandsParams {
+  production?: boolean;
+}
+export const getAllBands = async ({
+  production = false,
+}: GetAllBandsParams = {}): Promise<Band[] | null> => {
+  if (!production) return MOCK_BANDS.active;
+  try {
+    const res = await bandsCollection
+      .where({ status: _.eq("recruiting") })
+      .get();
+
+    handleDBResult(res, "get", "获取全部乐队数据");
+
     return res.data as Band[];
   } catch (error) {
     console.error(error);
@@ -32,5 +53,35 @@ export const getAllBands = async ({
   }
 };
 
+interface GetBandsByStatusParams {
+  status?: BandStatus;
+  production?: boolean;
+}
+export const getBandsByStatus = async ({
+  status,
+  production = false,
+}: GetBandsByStatusParams = {}): Promise<Band[] | null> => {
+  if (!production) return MOCK_BANDS["active"];
+
+  try {
+    let res: DB.Query.IQueryResult;
+    if (!status) {
+      res = await bandsCollection.get();
+    } else {
+      res = await bandsCollection.where({ status: _.eq(status) }).get();
+    }
+
+    handleDBResult(res, "get", "获取乐队数据");
+    return res.data as Band[];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+interface GetMyBandsParams {
+  userID: string;
+  production?: boolean;
+}
 /* UPDATE */
 /* DELETE */
