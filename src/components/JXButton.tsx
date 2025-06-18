0b9681f012d75b62b042f5408e80a9d3eae21c6e
notURandomDev <1250901577@qq.com>
2025-06-18 20:18:23 +0800

    fix：JXButton｜添加了更多的样式选择

--- src/components/JXButton.tsx
+++ src/components/JXButton.tsx
@@ -1,16 +1,30 @@
 import { Button } from "@taroify/core";
+import { ITouchEvent } from "@tarojs/components";
 import { ReactNode } from "react";
 
 interface JXButtonProps {
   children: ReactNode;
+  onClick?: (event: ITouchEvent) => void;
+  variant?: "solid" | "outlined";
+  disabled?: boolean;
 }
 
-function JXButton({ children }: JXButtonProps) {
+function JXButton({
+  children,
+  onClick,
+  variant = "solid",
+  disabled = false,
+}: JXButtonProps) {
+  const isSolid = variant === "solid";
+
   return (
     <Button
+      disabled={disabled}
+      onClick={onClick}
       style={{
-        backgroundColor: "#11181C",
-        color: "#fff",
+        borderColor: isSolid ? "#fff" : "#11181C",
+        backgroundColor: isSolid ? "#11181C" : "#f7f8fa",
+        color: isSolid ? "#fff" : "#11181C",
         padding: "10px 20px",
         height: "auto",
         fontWeight: 500,
