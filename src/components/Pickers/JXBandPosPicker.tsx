2946315c35add12d4b0f39e6e6e1d4b47439a297
notURandomDev <1250901577@qq.com>
2025-06-18 20:21:07 +0800

    feat：新增了用于选择乐队的组件：JXBandPosPicker

---
+++ src/components/Pickers/JXBandPosPicker.tsx
@@ -0,0 +1,44 @@
+import { MUSICIAN_DISPLAY, MusicianType } from "@/constants/utils/musician";
+import { Picker, Popup } from "@taroify/core";
+import { PickerOptionData } from "@taroify/core/picker/picker.shared";
+
+const BAND_POS_COLUMNS: PickerOptionData[] = Object.keys(MUSICIAN_DISPLAY).map(
+  (p) => ({
+    label: `${MUSICIAN_DISPLAY[p].emoji} ${MUSICIAN_DISPLAY[p].label}`,
+    value: p,
+  })
+);
+
+interface JXBandPosPickerProps {
+  title: string;
+  open: boolean;
+  onConfirm?: (position: MusicianType) => void;
+  onCancel?: () => void;
+}
+
+function JXBandPosPicker({
+  title = "选择乐手位置",
+  open,
+  onConfirm = () => {},
+  onCancel = () => {},
+}: JXBandPosPickerProps) {
+  const handleConfirm = (position: string) => {
+    onConfirm(position as MusicianType);
+  };
+
+  return (
+    <Popup open={open} rounded placement="bottom">
+      <Popup.Backdrop />
+      <Picker
+        title={title}
+        cancelText="取消"
+        confirmText="确认"
+        columns={BAND_POS_COLUMNS}
+        onCancel={onCancel}
+        onConfirm={(option) => handleConfirm(option[0])}
+      />
+    </Popup>
+  );
+}
+
+export default JXBandPosPicker;
