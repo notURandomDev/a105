2c3e64de0ee5bd592c125a889172a057a33fe7da
notURandomDev <1250901577@qq.com>
2025-06-20 00:09:17 +0800

    feat：新建乐手卡片｜在具体乐手分类的Tab中使用

--- src/components/Cards/JXMusicianCard.tsx
+++ src/components/Cards/JXMusicianCard.tsx
@@ -6,46 +6,28 @@ import JXBodyLabel from "../Labels/JXBodyLabel";
 import JXSecondaryLabel from "../Labels/JXSecondaryLabel";
 import JXTitleLabel from "../Labels/JXTitleLabel";
 
-function JXMusicianCard() {
+interface JXMusicianCardProps {
+  nickName: string;
+}
+
+function JXMusicianCard({ nickName = "Musician Name" }: JXMusicianCardProps) {
   return (
     <JXCardContainer style={{ gap: 8 }}>
       <View className="container-h" style={{ gap: 12, alignItems: "center" }}>
-        <JXAvatar style={{ height: 45, width: 45 }}>a</JXAvatar>
-        <View className="container-v" style={{ gap: 3 }}>
-          <JXTitleLabel>Kyle</JXTitleLabel>
-          <View className="chip-container">
-            <JXGenreChip genre="Blues" />
-          </View>
+        <JXAvatar>a</JXAvatar>
+        <View className="container-v">
+          <JXTitleLabel>{nickName}</JXTitleLabel>
+          <JXSecondaryLabel>TA 是 2 个乐队的吉他手</JXSecondaryLabel>
         </View>
+      </View>
+
+      <View className="chip-container">
+        <JXGenreChip genre="Blues" />
       </View>
 
       <JXBodyLabel>
         乐手简介：这是一段很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的乐手简介
       </JXBodyLabel>
-
-      <View
-        className="container-h grow"
-        style={{ justifyContent: "space-between" }}
-      >
-        <View
-          style={{ flex: 1, justifyContent: "flex-start" }}
-          className="container-h"
-        >
-          <JXSecondaryLabel>加入于：2025-06</JXSecondaryLabel>
-        </View>
-        <View
-          style={{ flex: 1, justifyContent: "center" }}
-          className="container-h"
-        >
-          <JXSecondaryLabel>所属乐队：3</JXSecondaryLabel>
-        </View>
-        <View
-          style={{ flex: 1, justifyContent: "flex-end" }}
-          className="container-h"
-        >
-          <JXSecondaryLabel>当前年级：大三</JXSecondaryLabel>
-        </View>
-      </View>
     </JXCardContainer>
   );
 }
