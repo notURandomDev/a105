ab02f9b0728493b075466efe823316fdfa085dbc
notURandomDev <1250901577@qq.com>
2025-07-26 12:18:13 +0800

    feat：添加收件箱界面

---
+++ src/pages/mail/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "@tarojs/components";
+import "./index.scss";
+
+export default function Mail() {
+  return (
+    <View className="mail">
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
