65e54bf6ca0f53c3722cc80e57a31510ba257e22
notURandomDev <1250901577@qq.com>
2025-08-02 17:04:56 +0800

    feat：更新「申请记录」实体，优化字段

--- src/models/application.ts
+++ src/models/application.ts
@@ -1,14 +1,19 @@
-import { BandPosition } from "./band-position";
-import { PositionType } from "./position";
-
 export type ApplicationStatus = "pending" | "approved" | "rejected";
 
 export interface Application {
   _id: string | number; // 唯一标识符
   appliedAt: Date; // 申请时间
   status: ApplicationStatus; // 申请状态
-  applicantName: string; // 申请人名称
-  applicantPosition: PositionType; // 申请人职位
-  bandName: string; // 乐队名称
-  bandPositions: BandPosition[];
+  applyingBandPositionID: string | number; // 申请加入的乐队位置ID
+  applyingMusicianID: string | number; // 申请人的乐手ID
+  targetBandID: string | number; // 申请加入乐队的ID
 }
+
+export type CreateApplicationInput = Pick<
+  Application,
+  | "appliedAt"
+  | "applyingMusicianID"
+  | "status"
+  | "targetBandID"
+  | "applyingBandPositionID"
+>;
