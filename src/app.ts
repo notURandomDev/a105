b6a4471af83101ecfa070580310eb9f31e84a46a
notURandomDev <1250901577@qq.com>
2025-09-14 14:00:40 +0800

    refactor：service层逆向重构｜取消主应用开启时除用户之外所有全局状态的获取

--- src/app.ts
+++ src/app.ts
@@ -3,31 +3,14 @@ import Taro, { useLaunch } from "@tarojs/taro";
 
 import "./app.scss";
 import { useUserStore } from "./stores/userStore";
-import { useBandStore } from "./stores/bandStore";
-import { useBandPositionStore } from "./stores/bandPositionStore";
-import { useReservationStore } from "./stores/reservationStore";
-import { useMusicianStore } from "./stores/musicianStore";
-import { useApplicationStore } from "./stores/applicationStore";
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
     console.log("JXNB！");
   });
 
+  // 获取用户全局状态
   const { userInfo } = useUserStore();
-  const fetchBands = useBandStore((s) => s.fetchBands);
-  const fetchBandPositions = useBandPositionStore((s) => s.fetchBandPositions);
-  const fetchReservations = useReservationStore((s) => s.fetchReservations);
-  const fetchMusicians = useMusicianStore((s) => s.fetchMusicians);
-  const fetchApplications = useApplicationStore((s) => s.fetchApplications);
-
-  useEffect(() => {
-    fetchBands();
-    fetchBandPositions();
-    fetchReservations();
-    fetchMusicians();
-    fetchApplications();
-  }, []);
 
   useEffect(() => {
     if (!userInfo) Taro.navigateTo({ url: "/pages/auth/index" });
