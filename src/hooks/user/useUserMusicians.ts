5f3286e4c1d1be38d7e68c2023392072133e3101
notURandomDev <1250901577@qq.com>
2025-09-27 14:48:40 +0800

    feat：useUserMusicians 支持 lazyload
    lazyLoad(默认关闭)开启情况下，不在钩子初始化时获取用户乐手数据。
    
    在这种情况下，useUserMusicians的价值在于，能够提供 useUserStore 的初始化，在调用 fetchUserMusicians 时能够直接返回用户乐手数据，无需在外部再进行 userInfo 数据的管理

--- src/hooks/user/useUserMusicians.ts
+++ src/hooks/user/useUserMusicians.ts
@@ -5,11 +5,12 @@ import { useEffect, useState } from "react";
 
 interface UseUserMusiciansProps {
   production?: boolean;
+  lazyLoad?: boolean;
 }
 
-export const useUserMusicians = ({
-  production = true,
-}: UseUserMusiciansProps = {}) => {
+export const useUserMusicians = (params: UseUserMusiciansProps = {}) => {
+  const { production = true, lazyLoad = false } = params;
+
   const { userInfo } = useUserStore();
   const [userMusicians, setUserMusicians] = useState<Musician[]>([]);
 
@@ -17,13 +18,16 @@ export const useUserMusicians = ({
   const userID = userInfo?._id;
 
   const fetchUserMusicians = async () => {
-    if (!userID) return;
+    if (!userID) return [];
     const musicians =
       (await getMusiciansByUserID({ userID, production })) || [];
     setUserMusicians(musicians);
+    return musicians;
   };
 
   useEffect(() => {
+    // 通常在只使用获取用户乐手数据能力时，开启 `lazyLoad`
+    if (lazyLoad) return;
     fetchUserMusicians();
   }, [userID]);
 
